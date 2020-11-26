fe05544bf0a5a1d73c8db8064dac2e8517222a52
keyoni <56132773+keyoni@users.noreply.github.com>
2020-11-26 15:19:54 -0800

    small updates with login

--- app/src/main/java/com/example/quicktrips/SignInFragment.kt
+++ app/src/main/java/com/example/quicktrips/SignInFragment.kt
@@ -6,21 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.quicktrips.databinding.FragmentSignInBinding
+import com.example.quicktrips.db.AppDatabase
+import com.example.quicktrips.db.AppViewModel
+import com.example.quicktrips.db.AppViewModelFactory
+import com.example.quicktrips.db.entites.User
+import com.example.quicktrips.db.repos.AppRepository
 import com.example.quicktrips.userscreens.UserActivity
 
 
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
     private lateinit var binding: FragmentSignInBinding
 
+    private lateinit var mViewModel: AppViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSignInBinding.bind(view)
 
+        val database = AppDatabase(requireContext())
+        val repository = AppRepository(database)
+        val factory = AppViewModelFactory(repository)
+        mViewModel = ViewModelProvider(this,factory).get(AppViewModel::class.java)
+
 
         binding.btnSignIn.setOnClickListener() {
-            //add UserID as extra then use to esxcaty  account profile page and if Doctor
+            //add UserID as extra then use to connect to account profile page and if Doctor for admin
+            val username = binding.etUserName.text.toString()
+            val userPassword = binding.etPassword.text.toString()
+//
+//            if(username != "" && userPassword != "") {
+//              //  val user: User? = signIn(username, userPassword)
+//            }
             val intent = Intent(activity,UserActivity::class.java).also {
                 startActivity(it)
             }
@@ -30,6 +49,13 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
             Navigation.findNavController(it).navigate(R.id.navigate_to_signUpFragment)
         }
     }
+//
+//    private fun signIn(username: String, userPassword: String) : User
+//    {
+//        val userID = mViewModel.getUserByNameAndPassword(username,userPassword)
+//        return user
+//
+//    }
 
 
 }
--- app/src/main/java/com/example/quicktrips/SignUpFragment.kt
+++ app/src/main/java/com/example/quicktrips/SignUpFragment.kt
@@ -1,40 +1,51 @@
 package com.example.quicktrips
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.quicktrips.databinding.FragmentSignUpBinding
 import com.example.quicktrips.db.AppDatabase
+import com.example.quicktrips.db.AppViewModel
+import com.example.quicktrips.db.AppViewModelFactory
 import com.example.quicktrips.db.UserDao
 import com.example.quicktrips.db.entites.User
+import com.example.quicktrips.db.repos.AppRepository
 
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
     private lateinit var binding: FragmentSignUpBinding
+    private lateinit var mViewModel: AppViewModel
+    var mUserDao : UserDao? = null
 
-    //var mUserDao : UserDao? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSignUpBinding.bind(view)
+
+        val database = AppDatabase(requireContext())
+        val repository = AppRepository(database)
+        val factory = AppViewModelFactory(repository)
+        mViewModel = ViewModelProvider(this,factory).get(AppViewModel::class.java)
+
+        // Code adapted from Stevdza-San (https://www.youtube.com/watch?v=UBCAWfztTrQ)
         binding.btnSignUp.setOnClickListener(){
             val username = binding.etUserName.text.toString()
-            val password = binding.etPassword.text.toString()
+            val userPassword = binding.etPassword.text.toString()
             val firstName = binding.etFirstName.text.toString()
             val lastName = binding.etLastName.text.toString()
             val isDoctor = binding.chDoctor.isChecked
-            Log.d("SIGNIN","$username, $password,$firstName,$lastName,$isDoctor")
-            if(username != "" && password != "" && firstName != "" && lastName != "" ) {
-                val newUser = User(username, password, firstName, lastName, isDoctor)
+
+            //Log.d("SIGNIN","$username, $password,$firstName,$lastName,$isDoctor")
+            if(username != "" && userPassword != "" && firstName != "" && lastName != "" ) {
+                val newUser = User(username, userPassword, firstName, lastName, isDoctor)
+                mViewModel.insert(newUser)
                 Toast.makeText(context,"Welcome to the TARDIS, please log in!",Toast.LENGTH_LONG).show()
                 Navigation.findNavController(it).navigate(R.id.navigate_to_signInFragment)
             } else {
-                Toast.makeText(context,"Please enter fill out all information!",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context,"Please enter all information!",Toast.LENGTH_SHORT).show()
             }
         }
     }
